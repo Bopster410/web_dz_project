150246275bbbccdcfe7e5cc806105b311c09dc9f
Bopster411 <serg.dr2004@gmail.com>
2023-11-07 09:57:27 +0300

    Added looping questions on the page template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    questions = [
+        {
+            'id': i,
+            'title': f'Question ({i})???',
+            'content': f'''Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident alias libero reprehenderit possimus, \
+dolore modi consequuntur placeat enim error suscipit vitae officiis iure in totam dignissimos nulla eius, quaerat aliquid ({i})?'''
+        } for i in range(10)
+    ]
+
+    return render(request, 'index.html', {'questions': questions})
+
 
 def hot_questions(request):
     return render(request, 'index_hot.html')
