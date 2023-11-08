465202efb6d4aa25c04c2275f153dcfbf895d88c
Bopster411 <serg.dr2004@gmail.com>
2023-11-08 12:17:14 +0300

    Finished index_tags.html template

--- app/views.py
+++ app/views.py
@@ -46,7 +46,7 @@ def tag(request, tag_name):
     # TODO send to another page if tag doesn't exist
     tag_item = TAGS[tag_name] if tag_name in TAGS else TAGS['Animals']
     questions = [QUESTIONS[i] for i in range(len(QUESTIONS)) if i % 3 == 0]
-    return render(request, 'index_tags.html', {'tag': tag_item, 'tags': TAGS.values(), 'questions': questions})
+    return render(request, 'index_tags.html', {'tag_item': tag_item, 'tags': TAGS.values(), 'questions': questions})
 
 # Question
 def question(request, question_id):
