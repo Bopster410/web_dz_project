b3a443f43c5f9ababa40a18d98d98eefeb108309
Bopster411 <serg.dr2004@gmail.com>
2023-11-08 11:43:35 +0300

    Added tags components to the questions

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ QUESTIONS = [
     {
         'id': i,
         'title': f'Question ({i})???',
+        'tags': [{'name': 'Python', 'bg': 'bg-primary'}, {'name': 'Animals', 'bg': 'bg-warning'}, {'name': 'Google', 'bg': 'bg-danger'}],
         'content': f'''Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident alias libero reprehenderit possimus, \
 dolore modi consequuntur placeat enim error suscipit vitae officiis iure in totam dignissimos nulla eius, quaerat aliquid ({i})?'''
     } for i in range(30)
@@ -42,7 +43,8 @@ def hot_questions(request):
 def tag(request, tag_name):
     # TODO send to another page if tag doesn't exist
     tag_item = TAGS[tag_name] if tag_name in TAGS else TAGS['Animals']
-    return render(request, 'index_tags.html', {'tag': tag_item, 'tags': TAGS.values()})
+    questions = [QUESTIONS[i] for i in range(len(QUESTIONS)) if i % 3 == 0]
+    return render(request, 'index_tags.html', {'tag': tag_item, 'tags': TAGS.values(), 'questions': questions})
 
 # Question
 def question(request, question_id):
